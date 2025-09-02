9d195b0186b64161c120ef332f9d504be220ab74
quan <quan_xiaoxiao@qq.com>
2025-09-02 16:43:38 +0800

    xxx

--- src/generateProjectPageRoutes.mjs
+++ src/generateProjectPageRoutes.mjs
@@ -1,4 +1,7 @@
+import { Buffer } from 'node:buffer';
+
 import { fetchActions } from '@quanxiaoxiao/fetch-action';
+import { encodeContentEncoding } from '@quanxiaoxiao/http-utils';
 import createError from 'http-errors';
 import _ from 'lodash';
 
@@ -15,8 +18,8 @@ const createRouteHandler = (
     if (!projectItem) {
       throw createError(404);
     }
-    if (!projectItem.resource.pageInfo) {
-      console.warn(`\`project:${projectItem.name}\` pageInfo is unconfig`);
+    if (!projectItem.resource?.pageInfo) {
+      console.warn(`Project "${projectItem.name}" pageInfo is not configured`);
       throw createError(403);
     }
     if (_.isPlainObject(projectItem.data)) {
@@ -25,53 +28,61 @@ const createRouteHandler = (
       }
       Object.assign(ctx.state, projectItem.data);
     }
+
     if (!_.isEmpty(projectItem.api)) {
-      const ret = await fetchActions(projectItem.api)({
+      const apiResult = await fetchActions(projectItem.api)({
         hosts,
         request: ctx.request,
       });
-      if (_.isPlainObject(ret)) {
+      if (_.isPlainObject(apiResult)) {
         if (!ctx.state) {
           ctx.state = {};
         }
-        Object.assign(ctx.state, ret);
+        Object.assign(ctx.state, apiResult);
       }
     }
     const { pageInfo } = projectItem.resource;
     const options = {
-      documentAttributeList: [...pageInfo.documentAttributeList],
-      bodyAttributeList: [...pageInfo.documentAttributeList],
-      scriptList: [...pageInfo.scriptList],
-      styleList: [...pageInfo.styleList],
-      linkList: [...pageInfo.linkList],
-      metaList: [...pageInfo.metaList],
-      elemList: [...pageInfo.elemList],
+      documentAttributeList: [...pageInfo.documentAttributeList || []],
+      bodyAttributeList: [...pageInfo.documentAttributeList || []],
+      scriptList: [...pageInfo.scriptList || []],
+      styleList: [...pageInfo.styleList || []],
+      linkList: [...pageInfo.linkList || []],
+      metaList: [...pageInfo.metaList || []],
+      elemList: [...pageInfo.elemList || []],
       title: projectItem.title ?? '',
     };
-    if (ctx.state) {
+    if (ctx.state && Object.keys(ctx.state).length > 0) {
       options.scriptList.unshift({
         content: `window.__STATE__=${JSON.stringify(ctx.state)};`,
       });
     }
     if (onPageRender) {
-      const ret = onPageRender(ctx);
-      if (ret) {
+      const renderResult = onPageRender(ctx);
+      if (renderResult) {
         Object.keys(options).forEach((keyName) => {
-          if (Object.hasOwnProperty.call(ret, keyName)) {
+          if (Object.hasOwnProperty.call(renderResult, keyName)) {
             if (keyName === 'title') {
-              options.title = ret[keyName];
-            } else if (Array.isArray(ret[keyName])) {
-              options[keyName].push(...ret[keyName]);
+              options.title = renderResult[keyName];
+            } else if (Array.isArray(renderResult[keyName])) {
+              options[keyName].push(...renderResult[keyName]);
             }
           }
         });
       }
     }
+    const content = renderToHtml(options);
+    const contentBuf = Buffer.from(content);
+    const encodedContentResult = encodeContentEncoding(contentBuf, ctx.request.headers['accept-encoding']);
+
     ctx.response = {
       headers: {
         'Content-Type': 'text/html; charset=utf-8',
+        ...encodedContentResult.name ? {
+          'Content-Encoding': encodedContentResult.name,
+        } : {},
       },
-      body: renderToHtml(options),
+      body: encodedContentResult.buf,
     };
   };
 };
