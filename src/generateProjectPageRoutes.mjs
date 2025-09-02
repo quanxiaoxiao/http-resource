ced0558f0d7f0a46eda6cb31c594d04eab555902
quan <quan_xiaoxiao@qq.com>
2025-09-02 16:17:25 +0800

    xxx

--- src/generateProjectPageRoutes.mjs
+++ src/generateProjectPageRoutes.mjs
@@ -4,6 +4,78 @@ import _ from 'lodash';
 
 import renderToHtml from './html/renderToHtml.mjs';
 
+const createRouteHandler = (
+  projectName,
+  hosts,
+  onPageRender,
+  getProject,
+) => {
+  return async (ctx) => {
+    const d = getProject(projectName);
+    if (!d) {
+      throw createError(404);
+    }
+    if (!d.resource.pageInfo) {
+      console.warn(`\`project:${d.name}\` pageInfo is unconfig`);
+      throw createError(403);
+    }
+    if (_.isPlainObject(d.data)) {
+      if (!ctx.state) {
+        ctx.state = {};
+      }
+      Object.assign(ctx.state, d.data);
+    }
+    if (!_.isEmpty(d.api)) {
+      const ret = await fetchActions(d.api)({
+        hosts,
+        request: ctx.request,
+      });
+      if (_.isPlainObject(ret)) {
+        if (!ctx.state) {
+          ctx.state = {};
+        }
+        Object.assign(ctx.state, ret);
+      }
+    }
+    const { pageInfo } = d.resource;
+    const options = {
+      documentAttributeList: [...pageInfo.documentAttributeList],
+      bodyAttributeList: [...pageInfo.documentAttributeList],
+      scriptList: [...pageInfo.scriptList],
+      styleList: [...pageInfo.styleList],
+      linkList: [...pageInfo.linkList],
+      metaList: [...pageInfo.metaList],
+      elemList: [...pageInfo.elemList],
+      title: d.title ?? '',
+    };
+    if (ctx.state) {
+      options.scriptList.unshift({
+        content: `window.__STATE__=${JSON.stringify(ctx.state)};`,
+      });
+    }
+    if (onPageRender) {
+      const ret = onPageRender(ctx);
+      if (ret) {
+        Object.keys(options).forEach((keyName) => {
+          if (Object.hasOwnProperty.call(ret, keyName)) {
+            if (keyName === 'title') {
+              options.title = ret[keyName];
+            } else if (Array.isArray(ret[keyName])) {
+              options[keyName].push(...ret[keyName]);
+            }
+          }
+        });
+      }
+    }
+    ctx.response = {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+      body: renderToHtml(options),
+    };
+  };
+};
+
 export default (
   {
     list,
@@ -15,74 +87,11 @@ export default (
   const routes = {};
 
   list.forEach((projectItem) => {
-    for (let j = 0; j < projectItem.list.length; j++) {
-      routes[projectItem.list[j]] = {
-        get: async (ctx) => {
-          const d = getProject(projectItem.name);
-          if (!d) {
-            throw createError(404);
-          }
-          if (!d.resource.pageInfo) {
-            console.warn(`\`project:${d.name}\` pageInfo is unconfig`);
-            throw createError(403);
-          }
-          if (_.isPlainObject(d.data)) {
-            if (!ctx.state) {
-              ctx.state = {};
-            }
-            Object.assign(ctx.state, d.data);
-          }
-          if (!_.isEmpty(d.api)) {
-            const ret = await fetchActions(d.api)({
-              hosts,
-              request: ctx.request,
-            });
-            if (_.isPlainObject(ret)) {
-              if (!ctx.state) {
-                ctx.state = {};
-              }
-              Object.assign(ctx.state, ret);
-            }
-          }
-          const { pageInfo } = d.resource;
-          const options = {
-            documentAttributeList: [...pageInfo.documentAttributeList],
-            bodyAttributeList: [...pageInfo.documentAttributeList],
-            scriptList: [...pageInfo.scriptList],
-            styleList: [...pageInfo.styleList],
-            linkList: [...pageInfo.linkList],
-            metaList: [...pageInfo.metaList],
-            elemList: [...pageInfo.elemList],
-            title: d.title ?? '',
-          };
-          if (ctx.state) {
-            options.scriptList.unshift({
-              content: `window.__STATE__=${JSON.stringify(ctx.state)};`,
-            });
-          }
-          if (onPageRender) {
-            const ret = onPageRender(ctx);
-            if (ret) {
-              Object.keys(options).forEach((keyName) => {
-                if (Object.hasOwnProperty.call(ret, keyName)) {
-                  if (keyName === 'title') {
-                    options.title = ret[keyName];
-                  } else if (Array.isArray(ret[keyName])) {
-                    options[keyName].push(...ret[keyName]);
-                  }
-                }
-              });
-            }
-          }
-          ctx.response = {
-            headers: {
-              'Content-Type': 'text/html; charset=utf-8',
-            },
-            body: renderToHtml(options),
-          };
-        },
+    projectItem.list.forEach((route) => {
+      routes[route] = {
+        get: createRouteHandler(projectItem.name, hosts, onPageRender, getProject),
       };
-    }
+    });
   });
 
   return routes;
