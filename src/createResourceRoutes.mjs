4fa7736986f0e5f3875c74c8453031fdbacefb9a
quan <quan_xiaoxiao@qq.com>
2025-09-02 16:10:33 +0800

    xxx

--- src/createResourceRoutes.mjs
+++ src/createResourceRoutes.mjs
@@ -10,90 +10,109 @@ import * as tar from 'tar';
 import readProjectResources from './readProjectResources.mjs';
 import storeProjectResources from './storeProjectResources.mjs';
 
+const DEFAULT_RESOURCE_DIST_PREFIX = '/static';
+const DEFAULT_RESOURCE_UPDATE_PREFIX = '/www';
+const TEMP_DIR_CLEANUP_DELAY = 1000;
+const FORBIDDEN_PATHS = new Set(['index.html']);
+
 export default (
   {
-    resourceDistPrefix = '/static',
-    resourceUpdatePrefix = '/www',
+    resourceDistPrefix = DEFAULT_RESOURCE_DIST_PREFIX,
+    resourceUpdatePrefix = DEFAULT_RESOURCE_UPDATE_PREFIX,
     logger,
     onUpdateResource,
   },
   getProject,
-) => ({
-  [`${resourceUpdatePrefix}/:name/:key`]: {
-    onPre: (ctx) => {
-      const projectItem = getProject(ctx.request.params.name);
-      if (!projectItem) {
-        throw createError(404);
-      }
-      if (projectItem.key !== ctx.request.params.key) {
-        throw createError(401);
-      }
-      if (ctx.request.method === 'POST') {
-        if (!hasHttpBodyContent(ctx.request.headers)) {
-          throw createError(403);
+) => {
+  if (typeof getProject !== 'function') {
+    throw new Error('getProject must be a function');
+  }
+
+  return {
+    [`${resourceUpdatePrefix}/:name/:key`]: {
+      onPre: (ctx) => {
+        const { name, key } = ctx.request.params;
+        const projectItem = getProject(name);
+        if (!projectItem) {
+          throw createError(404);
+        }
+        if (projectItem.key !== key) {
+          throw createError(401);
+        }
+        if (ctx.request.method === 'POST') {
+          if (!hasHttpBodyContent(ctx.request.headers)) {
+            throw createError(403);
+          }
+          if (logger) {
+            logger.warn(`\`project:${projectItem.name}\` will update resources...`);
+          }
+          const resourceTempDir = path.resolve(projectItem.dir, projectItem.tempDirName);
+
+          if (shelljs.test('-d', resourceTempDir)) {
+            shelljs.rm('-rf', resourceTempDir);
+          }
+
+          shelljs.mkdir('-p', resourceTempDir);
+
+          ctx.request.body = new PassThrough();
+          ctx.request.body.pipe(tar.x({
+            strip: 1,
+            C: resourceTempDir,
+          }));
+        }
+      },
+      post: async (ctx) => {
+        const projectItem = getProject(ctx.request.params.name);
+        await waitFor(TEMP_DIR_CLEANUP_DELAY);
+        const ret = storeProjectResources(projectItem);
+        if (!ret) {
+          throw createError(500);
         }
         if (logger) {
-          logger.warn(`\`project:${projectItem.name}\` will update resources...`);
+          logger.warn(`\`project:${projectItem.name}\` update resources success, \`size:${ret.size}\` \`hash:${ret.hash}\``);
         }
-        const resourceTempDir = path.resolve(projectItem.dir, projectItem.tempDirName);
-
-        if (shelljs.test('-d', resourceTempDir)) {
-          shelljs.rm('-rf', resourceTempDir);
-        }
-
-        shelljs.mkdir('-p', resourceTempDir);
-
-        ctx.request.body = new PassThrough();
-        ctx.request.body.pipe(tar.x({
-          strip: 1,
-          C: resourceTempDir,
-        }));
-      }
-    },
-    post: async (ctx) => {
-      const projectItem = getProject(ctx.request.params.name);
-      await waitFor(1000);
-      const ret = storeProjectResources(projectItem, logger);
-      if (!ret) {
-        throw createError(403);
-      }
-      if (logger) {
-        logger.warn(`\`project:${projectItem.name}\` update resources success, \`size:${ret.size}\` \`hash:${ret.hash}\``);
-      }
-      ctx.response = {
-        data: ret,
-      };
-      process.nextTick(() => {
+        ctx.response = {
+          data: ret,
+        };
         if (onUpdateResource) {
-          const d = getProject(ctx.request.params.name);
-          onUpdateResource({
-            ...d,
-            resource: readProjectResources(d),
+          process.nextTick(() => {
+            const updatedProject = getProject(ctx.request.params.name);
+            if (updatedProject) {
+              onUpdateResource({
+                ...updatedProject,
+                resource: readProjectResources(updatedProject),
+              });
+            }
           });
         }
-      });
+      },
     },
-  },
-  [`${resourceDistPrefix}/:name{/*path}`]: {
-    get: (ctx) => {
-      const projectItem = getProject(ctx.request.params.name);
-      if (!projectItem) {
-        throw createError(404);
-      }
-      const resourceItem = projectItem.resource.list.find((d) => d.pathname === ctx.request.params.path);
-      if (!resourceItem) {
-        throw createError(404);
-      }
-      if (ctx.request.params.path === 'index.html') {
-        throw createError(403);
-      }
-      const etag = ctx.request.headers['if-none-match'];
-      if (etag && etag === resourceItem.hash) {
-        ctx.response = {
-          statusCode: 304,
-          body: null,
-        };
-      } else {
+
+    [`${resourceDistPrefix}/:name{/*path}`]: {
+      get: (ctx) => {
+        const { name, path: requestPath } = ctx.request.params;
+        const projectItem = getProject(name);
+        if (!projectItem) {
+          throw createError(404);
+        }
+        if (FORBIDDEN_PATHS.has(requestPath)) {
+          throw createError(403);
+        }
+
+        const resourceItem = projectItem?.resource?.list?.find((d) => d.pathname === requestPath);
+
+        if (!resourceItem) {
+          throw createError(404);
+        }
+
+        const clientEtag = ctx.request.headers['if-none-match'];
+        if (clientEtag && clientEtag === resourceItem.hash) {
+          ctx.response = {
+            statusCode: 304,
+            body: null,
+          };
+          return;
+        }
         ctx.response = {
           headers: {
             Etag: resourceItem.hash,
@@ -103,11 +122,14 @@ export default (
         if (resourceItem.mime) {
           ctx.response.headers['Content-Type'] = resourceItem.mime;
         }
-        if (/\bgzip\b/i.test(ctx.request.headers['accept-encoding'])) {
+
+        const supportsGzip = /\bgzip\b/i.test(ctx.request.headers['accept-encoding']);
+
+        if (supportsGzip && resourceItem.bufGzip) {
           ctx.response.headers['Content-Encoding'] = 'gzip';
           ctx.response.body = resourceItem.bufGzip;
         }
-      }
+      },
     },
-  },
-});
+  };
+};
